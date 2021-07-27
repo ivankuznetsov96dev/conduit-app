22c82ead1f4206a713fbfaa6571e7a33f2194415
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2021-07-27 11:36:36 +0300

    fix: fix commit change detection

--- src/app/auth/store/actions/login.action.ts
+++ src/app/auth/store/actions/login.action.ts
@@ -19,6 +19,7 @@ export const loginFailureAction = createAction(
   props<{errors: BackendErrorsInterface}>(),
 );
 
+
 // export interface WrapperActionInteface {
 //   request: LoginRequestInterface;
 // }
